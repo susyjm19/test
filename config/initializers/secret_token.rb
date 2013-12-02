bdb19f82c41ff40850212926abf6331d877f8d5c
Digisolution <digisolution@ubuntu.(none)>
2013-12-02 01:03:15 -0500

    x

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '0f42ddbe7dcd9c91dcc7882fcac29241a596e7a2368644a9787c3b4deafe247cade297a59a039f30a06841360321a73ca1a884c678778dfc82cab6fa13550ea4'
